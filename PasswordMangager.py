fb91d3faf2c9aedee8b97e00f5f6e37ec42ac698
gruv0o <renaultremi51100@gmail.com>
2024-05-29 13:04:12 +0200

    Adding: + Generate_key() function

--- PasswordMangager.py
+++ PasswordMangager.py
@@ -2,9 +2,19 @@ import sqlite3
 import string
 import secrets
 import win32crypt
+import bcrypt
+
 from cryptography.fernet import Fernet
 
 #Génération de clé de chiffrement qui sera stocké
+def Generate_key():
+    key = Fernet.generate_key()
+
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+Generate_key()
+
 #Chiffrement d'un mot de passe choisi par l'utilisateur
 #création et chiffrement d'un mot de passe généré
 #création, hashage et stockage du master password
