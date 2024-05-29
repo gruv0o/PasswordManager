5e80574ba340d67a0c25f3c9c69d981f6114e202
gruv0o <renaultremi51100@gmail.com>
2024-05-29 12:39:30 +0200

    ds

---
+++ PasswordMangager.py
@@ -0,0 +1,13 @@
+import sqlite3
+import string
+import secrets
+import win32crypt
+from cryptography.fernet import Fernet
+d
+#Génération de clé de chiffrement qui sera stocké
+#Chiffrement d'un mot de passe choisi par l'utilisateur
+#création et chiffrement d'un mot de passe généré
+#création, hashage et stockage du master password
+#stockage des mot de passe chiffré
+#lecture et déchiffrement
+
